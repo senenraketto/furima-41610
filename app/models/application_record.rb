2a1e06f2e56d47ccfa9371dbae85d306aa770e43
senenraketto <yantianhaoiwatasora@gmail.com>
2026-06-11 15:19:56 +0900

    誤った記述の修正

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
-
-class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-end
