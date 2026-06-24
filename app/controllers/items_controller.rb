6d60ecb5ac9092afeacd4d61384077b8b1956dee
senenraketto <yantianhaoiwatasora@gmail.com>
2026-06-24 22:51:49 +0900

    ログインなどの有無の before_actionを用いた制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -38,5 +42,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
