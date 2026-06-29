6494ab4bd92471b2571b72a8c9be7e558fb816c5
senenraketto <yantianhaoiwatasora@gmail.com>
2026-06-29 10:47:21 +0900

    ROBCOP実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update ,:destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
   before_action :set_item, only: [:edit, :update, :show, :destroy]
-  before_action :contributor_confirmation, only: [:edit, :update ,:destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
