051f74b26e8e9ba0ed3e2d892e2acce670983b8d
senenraketto <yantianhaoiwatasora@gmail.com>
2026-06-29 10:45:37 +0900

    beforeアクションを用いた処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update]
-  before_action :set_item, only: [:edit, :update]
-  before_action :contributor_confirmation, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update ,:destroy]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update ,:destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -35,7 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
     redirect_to root_path
   end
