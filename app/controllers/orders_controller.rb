5c3b99ed6e3903f42739beec7d01d1cb4f59b0c7
senenraketto <yantianhaoiwatasora@gmail.com>
2026-07-02 20:56:43 +0900

    購入機能　保存処理

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,30 @@
+class OrdersController < ApplicationController
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save 
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
+end
