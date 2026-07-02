86fad61e472d2d3b307917be16ad405930f2b53b
senenraketto <yantianhaoiwatasora@gmail.com>
2026-07-02 21:33:58 +0900

    出品を行う際の単体テストコード

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,84 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe OrderAddress, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+  end
+
+  describe '商品購入機能' do
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できる' do
+        expect(@order_address).to be_valid
+      end
+      it 'buildingは空でも保存できる' do
+        @order_address.building = ''
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'token（カード情報）が空だと保存できない' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("トークンを入力してください")
+      end
+      it 'postal_codeが空だと保存できない' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("郵便番号を入力してください")
+      end
+      it 'postal_codeが「3桁ハイフン4桁」の形式でないと保存できない' do
+        @order_address.postal_code = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("郵便番号はハイフン（-）を含めて入力してください")
+      end
+      it 'prefecture_idを選択していない（値が1の場合）保存できない' do
+        @order_address.prefecture_id = 1
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("都道府県を選択してください")
+      end
+      it 'cityが空だと保存できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("市区町村を入力してください")
+      end
+      it 'addressesが空だと保存できない' do
+        @order_address.addresses = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("番地を入力してください")
+      end
+      it 'phone_numberが空だと保存できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("電話番号を入力してください")
+      end
+      it 'phone_numberが9桁以下だと保存できない' do
+        @order_address.phone_number = '090123456'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("電話番号は10桁以上11桁以内の半角数値で入力してください")
+      end
+      it 'phone_numberが12桁以上だと保存できない' do
+        @order_address.phone_number = '090123456789'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("電話番号は10桁以上11桁以内の半角数値で入力してください")
+      end
+      it 'phone_numberに半角数字以外（ハイフンなど）が含まれていると保存できない' do
+        @order_address.phone_number = '090-1234-56'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("電話番号は10桁以上11桁以内の半角数値で入力してください")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Userを入力してください")
+      end
+      it 'itemが紐付いていないと保存できない' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Itemを入力してください")
+      end
+    end
+  end
 end
