312727a8e0468e19b43f0aada89883981e3636c0
senenraketto <yantianhaoiwatasora@gmail.com>
2026-07-09 10:27:58 +0900

    rubcop　実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,23 +5,23 @@ class OrdersController < ApplicationController
 
   def index
     @order_address = OrderAddress.new
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
 
-    if @order_address.valid? 
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    if @order_address.valid?
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
       Payjp::Charge.create(
-        amount: @item.price, 
-        card: order_params[:token], 
+        amount: @item.price,
+        card: order_params[:token],
         currency: 'jpy'
       )
-      @order_address.save 
+      @order_address.save
       redirect_to root_path
     else
-      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
@@ -29,11 +29,12 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
 end
-
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,5 +1,4 @@
 class OrderAddress
-
   include ActiveModel::Model
 
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
@@ -7,11 +6,11 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code,   format: { with: /\A\d{3}-\d{4}\z/, message: "はハイフン（-）を含めて入力してください" }
-    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :postal_code,   format: { with: /\A\d{3}-\d{4}\z/, message: 'はハイフン（-）を含めて入力してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
     validates :city
     validates :address
-    validates :phone_number,  format: { with: /\A\d{10,11}\z/, message: "は10桁または11桁の半角数字で入力してください" }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は10桁または11桁の半角数字で入力してください' }
   end
 
   # ④ 各テーブルにデータを保存する処理を書く
@@ -24,7 +23,7 @@ class OrderAddress
       addresses: address,
       building: building,
       phone_number: phone_number,
-      order_id: order.id 
+      order_id: order.id
     )
   end
 end
